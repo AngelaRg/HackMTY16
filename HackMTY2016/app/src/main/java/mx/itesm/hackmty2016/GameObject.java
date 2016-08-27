96e62733f248cfc95cfaaf5510e66e8c2162cedc
AngelaRg <angela_9412@hotmail.com>
2016-08-27 13:50:13 -0500

    Codigo base del juego. Imagenes de prueba.

---
+++ HackMTY2016/app/src/main/java/mx/itesm/hackmty2016/GameObject.java
@@ -0,0 +1,45 @@
+package mx.itesm.hackmty2016;
+
+/**
+ * Created by Angela on 27/08/2016.
+ */
+import android.graphics.Rect;
+
+public abstract class GameObject {
+    protected int x;
+    protected int y;
+    protected int dy;
+    protected int dx;
+    protected int width;
+    protected int height;
+
+    public void setX(int x)
+    {
+        this.x = x;
+    }
+    public void setY(int y)
+    {
+        this.y = y;
+    }
+    public int getX()
+    {
+        return x;
+    }
+    public int getY()
+    {
+        return y;
+    }
+    public int getHeight()
+    {
+        return height;
+    }
+    public int getWidth()
+    {
+        return width;
+    }
+    public Rect getRectangle()
+    {
+        return new Rect(x, y, x+width, y+height);
+    }
+
+}
