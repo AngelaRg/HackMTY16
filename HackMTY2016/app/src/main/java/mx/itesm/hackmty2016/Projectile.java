9cd2ece267dfdf449368e6f9c15b9fd28a083444
HugoLG <hugo.leon.ga@gmail.com>
2016-08-28 01:06:22 -0500

    shot action added

--- HackMTY2016/app/src/main/java/mx/itesm/hackmty2016/Projectile.java
+++ HackMTY2016/app/src/main/java/mx/itesm/hackmty2016/Projectile.java
@@ -3,25 +3,49 @@ package mx.itesm.hackmty2016;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 
+import java.util.Random;
+
 /**
  * Created by hlg on 27/08/16.
  */
 public class Projectile extends GameObject {
+    float velocity;
     int height;
     int width;
     float slope;
+    private Bitmap spritesheet;
     private Animation animation = new Animation();
 
-    public Projectile (Bitmap res, int w, int h, float m, int numFrames, float initX, float initY) {
+    public Projectile (Bitmap res, int w, int h, float m, int numFrames, float initX, float initY, float vel) {
         this.vectorPosition = new Vector2D(initX, initY);
         width = w;
         height = h;
         slope = m;
+        velocity = vel;
+        resetBitmap(res, numFrames);
+    }
+
+    public void resetBitmap(Bitmap res, int numFrames){
+        Bitmap[] image = new Bitmap[numFrames];
+        spritesheet = res;
+        for (int i = 0; i < image.length; i++) {
+            image[i] = Bitmap.createBitmap(spritesheet, i*width, 0, width, height);
+        }
+        animation.setFrames(image);
+        animation.setDelay(80);
     }
 
     public void update() {
-        vectorPosition.setX(vectorPosition.getX()+this.slope);
-        vectorPosition.setY(vectorPosition.getY()+this.slope);
+        //Random rnum = new Random();
+        float adjustFactor = 0.1f;
+        if(slope > 0) {
+            vectorPosition.setX(vectorPosition.getX()-this.slope*velocity*adjustFactor);
+            vectorPosition.setY(vectorPosition.getY()-this.slope*velocity*adjustFactor);
+        } else {
+            vectorPosition.setX(vectorPosition.getX()-this.slope*velocity*adjustFactor);
+            vectorPosition.setY(vectorPosition.getY()+this.slope*velocity*adjustFactor);
+        }
+
     }
 
     public void setSlope(float m) {
